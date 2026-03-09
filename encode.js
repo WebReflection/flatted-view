b7a758f913b062b14be3c3d86b3a33faa8d6ac2a
webreflection <andrea.giammarchi@gmail.com>
2026-03-09 23:02:20 +0100

    probably better to fail at creation but never on revival?

--- encode.js
+++ encode.js
@@ -12,62 +12,91 @@ const I16 = U16 / 2;
 const I32 = U32 / 2;
 
 const { isArray } = Array;
+const { isView } = ArrayBuffer;
 const { isInteger } = Number;
 const { keys } = Object;
 
 const encoder = new TextEncoder;
-const placeholder = [];
 
-const crawl = ({ o, k, r }, data, type) => {
+const crawl = (o, k, data, type) => {
   switch (type) {
     case 'boolean':
-      o.push([data ? TRUE : FALSE]);
+      o.push(data ? TRUE : FALSE);
       break;
     case 'string':
-      if (k.has(data))
-        o.push(uint(RECURSION, k.get(data)));
-      else {
-        k.set(data, index(o));
-        const bytes = encoder.encode(data);
-        o.push(uint(STRING, bytes.length), bytes);
-      }
+      string(o, k, data);
       break;
     case 'number':
-      o.push(number(data));
+      number(o, data);
       break;
     case 'object':
       if (!data)
-        o.push([NULL]);
+        o.push(NULL);
       else if (k.has(data))
-        o.push(uint(RECURSION, k.get(data)));
+        o.push(...uint(RECURSION, k.get(data)));
       else {
-        r.push({ i: o.length, data });
-        k.set(data, index(o));
-        o.push(placeholder);
+        k.set(data, o.length);
+        if (isArray(data)) {
+          const length = data.length;
+          o.push(...uint(ARRAY, length));
+          for (let index = 0; index < length; index++) {
+            const value = data[index];
+            const type = typeof value;
+            if (valid(type)) crawl(o, k, value, type);
+            else o.push(NULL);
+          }
+        }
+        else {
+          const own = keys(data).filter(compatible, data);
+          const length = own.length;
+          o.push(...uint(OBJECT, length));
+          for (let index = 0; index < length; index++) {
+            const key = own[index];
+            string(o, k, key);
+            const value = data[key];
+            crawl(o, k, value, typeof value);
+          }
+        }
       }
       break;
   }
 };
 
-const r = (i, array) => i + array.length;
-const index = array => array.reduce(r, 0);
-
-const number = value => {
-  const asInteger = isInteger(value);
-  if (asInteger && value < I8 && -I8 <= value) {
-    dv.setInt8(0, value, true);
-    return [NUMBER | L8, v8[0]];
-  }
-  if (asInteger && value < I16 && -I16 <= value) {
-    dv.setInt16(0, value, true);
-    return [NUMBER | L16, v8[0], v8[1]];
-  }
-  if (asInteger && value < I32 && -I32 <= value) {
-    dv.setInt32(0, value, true);
-    return [NUMBER | L32, v8[0], v8[1], v8[2], v8[3]];
-  }
+const floating = (o, value) => {
   dv.setFloat64(0, value, true);
-  return [NUMBER | L64, ...v8];
+  o.push(NUMBER | L64, ...v8);
+};
+
+const number = (o, value) => {
+  if (isInteger(value)) {
+    if (value < I8 && -I8 <= value) {
+      dv.setInt8(0, value, true);
+      o.push(NUMBER | L8, v8[0]);
+    }
+    else if (value < I16 && -I16 <= value) {
+      dv.setInt16(0, value, true);
+      o.push(NUMBER | L16, v8[0], v8[1]);
+    }
+    else if (value < I32 && -I32 <= value) {
+      dv.setInt32(0, value, true);
+      o.push(NUMBER | L32, v8[0], v8[1], v8[2], v8[3]);
+    }
+    else floating(o, value);
+  }
+  else floating(o, value);
+};
+
+const string = (o, k, data) => {
+  if (k.has(data))
+    o.push(...uint(RECURSION, k.get(data)));
+  else {
+    const bytes = encoder.encode(data);
+    const length = bytes.length;
+    k.set(data, o.length);
+    o.push(...uint(STRING, length));
+    for (let i = 0; i < length; i += I16)
+      o.push(...bytes.subarray(i, i + I16));
+  }
 };
 
 const uint = (type, length) => {
@@ -97,46 +126,11 @@ const valid = type => {
   }
 };
 
-export const encode = data => {
-  let next = 0;
-  const o = [];
-  const r = [];
-  const okr = { o, k: new Map, r };
-  crawl(okr, data, typeof data);
-  while (next < r.length) {
-    const { i, data } = r[next++];
-    if (isArray(data)) {
-      const length = data.length;
-      o[i] = uint(ARRAY, length);
-      for (let index = 0; index < length; index++) {
-        const value = data[index];
-        const type = typeof value;
-        if (valid(type))
-          crawl(okr, value, type);
-        else
-          o.push([NULL]);
-      }
-    }
-    else {
-      const own = keys(data).filter(compatible, data);
-      const length = own.length;
-      o[i] = uint(OBJECT, length);
-      for (let index = 0; index < length; index++) {
-        const key = own[index];
-        crawl(okr, key, 'string');
-        const value = data[key];
-        crawl(okr, value, typeof value);
-      }
-    }
-  }
-  return Uint8Array.from(flatten(o));
+export const encode = (data, array = []) => {
+  crawl(array, new Map, data, typeof data);
+  return isView(array) ? array : new Uint8Array(array);
 };
 
 function compatible(key) {
   return valid(typeof this[key]);
-}
-
-function* flatten(array) {
-  for (let i = 0; i < array.length; i++)
-    yield* array[i];
 }
